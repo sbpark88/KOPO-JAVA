36bb73a08e767a952935abd4a583a961adbbcb75
sbpark88 <saebyulpark88@gmail.com>
2020-09-24 01:10:24 +0900

    Database directory and table name information move from HomeController to DB class file

---
+++ Study 09.16.1 Memo/src/main/java/com/kopo/memo/CommonController.java
@@ -0,0 +1,103 @@
+package com.kopo.memo;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CommonController {
+	
+	DB db = new DB(new Memo());
+	DB db2 = new DB(new People());
+	
+	// create table
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public String create(Locale locale, Model model) {		
+		if (db.open()) {
+			if (db.createTable()) {
+				model.addAttribute("message", "테이블이 생성되었습니다.");
+			} else {
+				model.addAttribute("message", "테이블 생성에 실패하였습니다.");
+			}
+			db.close();
+		} else {
+			model.addAttribute("message", "DB파일을 사용할 수 없습니다.");
+		}
+		
+		return "message";
+	}
+	
+	// 이런식으로 DB를 2개 만들어 2개 이상의 테이블을 사용할 수 있다.
+	@RequestMapping(value = "/create2", method = RequestMethod.GET)
+	public String create2(Locale locale, Model model) {		
+		if (db2.open()) {
+			if (db2.createTable()) {
+				model.addAttribute("message", "테이블이 생성되었습니다.");
+			} else {
+				model.addAttribute("message", "테이블 생성에 실패하였습니다.");
+			}
+			db2.close();
+		} else {
+			model.addAttribute("message", "DB파일을 사용할 수 없습니다.");
+		}
+		
+		return "message";
+	}
+	
+	// insert
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	public String insert(Locale locale, Model model, HttpServletRequest request) {
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		if (db.open()) {
+			if (db.insertData(request)) {				
+				model.addAttribute("message", "새 데이터를 추가했습니다.");
+			} else {
+				model.addAttribute("message", "데이터 추가에 실패했습니다.");
+			}
+			db.close();
+		} else {
+			model.addAttribute("message", "DB파일을 사용할 수 없습니다.");
+		}
+		
+		return "message";
+	}
+	
+	// update
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(Locale locale, Model model, HttpServletRequest request) {
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+	//		if (request.getParameter("idx") == null) {
+	//			model.addAttribute("message", "입력 데이터가 잘못 되었습니다.");
+	//			return "message";
+	//		}
+		
+		if (db.open()) {
+			if (db.updateData(request)) {				
+				model.addAttribute("message", "데이터를 수정했습니다.");
+			} else {
+				model.addAttribute("message", "데이터 수정에 실패했습니다.");
+			}
+			db.close();
+		} else {
+			model.addAttribute("message", "DB파일을 사용할 수 없습니다.");
+		}
+		
+		return "message";
+	}
+}
---
+++ Study 09.16.1 Memo/src/main/webapp/WEB-INF/views/message.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+
+<head>
+	<title>Message</title>
+</head>
+<body>
+
+<P>${message}</P>
+</body>
+</html>
